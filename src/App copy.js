091b039a4378411873e001c2e61dd0adff31bab0
Pitso Ntise <pitso.ntise@tibsonline.co.za>
2024-05-30 06:45:19 +0200

    Add Tibs Procure Web page received from Nomonde.

---
+++ src/App copy.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import tibsprocurebackground from './tibs-procure-background.jpg';
+import tibsprocurelogo from './tibs-procure-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V3 during standup.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
